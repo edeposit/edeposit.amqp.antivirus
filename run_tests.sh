14f4ec252f500e94f4aaee5ab92e647bed4c6bdf
Bystroushaak <bystrousak@kitakitsune.org>
2014-06-20 17:04:52 +0200

    Added run_tests.sh.

---
+++ run_tests.sh
@@ -0,0 +1,63 @@
+#! /usr/bin/env bash
+
+PYTHONPATH="$PYTHONPATH:src/"
+TEST_PATH="src/edeposit/amqp/antivirus/tests"
+
+function show_help {
+    echo -e "Usage: $0 [-h] [-a] [-i] [-u]"
+    echo
+    echo -e "\t-h"
+    echo -e "\t\tShow this help."
+    echo -e "\t-a"
+    echo -e "\t\tRun all tests."
+    echo -e "\t-i"
+    echo -e "\t\tRun integration test (requires sudo)."
+    echo -e "\t-u"
+    echo -e "\t\tRun unittest."
+    echo
+    exit;
+}
+
+function run_all_tests {
+    py.test $TEST_PATH;
+    exit
+}
+
+function run_int_tests {
+    py.test "$TEST_PATH/integration";
+    exit
+}
+
+function run_unit_tests {
+    py.test "$TEST_PATH/unittests";
+    exit
+}
+
+while getopts "haiu" optname; do
+    case "$optname" in
+        "a")
+            run_all_tests;
+        ;;
+        "i")
+            run_int_tests;
+        ;;
+        "u")
+            run_unit_tests;
+        ;;
+        "h")
+            show_help;
+        ;;
+        "?")
+            echo "Unknown option $OPTARG"
+        ;;
+        ":")
+            echo "No argument value for option $OPTARG"
+        ;;
+        *)
+            # Should not occur
+            echo "Unknown error while processing options"
+        ;;
+    esac
+done
+
+show_help;
