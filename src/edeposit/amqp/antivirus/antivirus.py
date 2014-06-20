fc89732841fc9aee9a10d5ea7e572ae9fb322b96
Bystroushaak <bystrousak@kitakitsune.org>
2014-06-20 15:40:22 +0200

    antivirus.py done.
    Bump #1.

--- src/edeposit/amqp/antivirus/antivirus.py
+++ src/edeposit/amqp/antivirus/antivirus.py
@@ -4,17 +4,29 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
-
-
-
-# Variables ===================================================================
-
+import wrappers.clamd as clamd
+import wrappers.clamscan as clamscan
+import settings
 
 
 # Functions & objects =========================================================
+def scan_file(path):
+    """
+    Scan `path` for viruses using ``clamd`` or ``clamscan`` (depends on
+    :attr:`settings.USE_CLAMD`.
 
+    Args:
+        path (str): Relative or absolute path of file/directory you need to
+                    scan.
 
+    Returns:
+        dict: ``{filename: ("FOUND", "virus type")}`` or blank dict.
 
-# Main program ================================================================
-if __name__ == '__main__':
-    pass
+    Raises:
+        ValueError: When the server is not running.
+        AssertionError: When the internal file doesn't exists.
+    """
+    if settings.USE_CLAMD:
+        return clamd.scan_file(path)
+    else:
+        return clamscan.scan_file(path)
