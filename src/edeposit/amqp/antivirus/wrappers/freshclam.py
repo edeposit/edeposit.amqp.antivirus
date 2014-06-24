d4c45682e5a42749d6dafed05b6555fc1e26124e
Bystroushaak <bystrousak@kitakitsune.org>
2014-06-24 12:15:10 +0200

    Added wrappers/freshclam.py.

---
+++ src/edeposit/amqp/antivirus/wrappers/freshclam.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+import os
+from functools import wraps
+
+
+import sh
+
+
+# Functions & objects =========================================================
+def require_root(fn):
+    """
+    Decorator to make sure, that user is root.
+    """
+    @wraps(fn)
+    def xex(*args, **kwargs):
+        assert os.geteuid() == 0, \
+            "You have to be root to run function '%s'." % fn.__name__
+        return fn(*args, **kwargs)
+
+    return xex
+
+
+@require_root
+def update_database():
+    return sh.freshclam()
