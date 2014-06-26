a75964e8167f45489ec833da20167a2bca0f3ed9
Bystroushaak <bystrousak@kitakitsune.org>
2014-06-26 16:45:22 +0200

    setup.py filled with required data for package creation.
    Bump #12.

--- setup.py
+++ setup.py
@@ -1,19 +1,99 @@
-Ubuntu packages: clamav clamav-daemon
-Suse packages: clamav
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Ubuntu packages: clamav clamav-daemon
+# Suse packages: clamav
 
-Ubuntu service: clamav-daemon
-Suse service: clamd
+import os
+import os.path
+import shutil
 
-Jednou za čas: freshclam pro update signatur.
+from setuptools import setup, find_packages
+from distutils.command.sdist import sdist
 
-pip install http://xael.org/norman/python/pyclamd/pyClamd-0.3.4.tar.gz
+try:
+    from docs import getVersion
+except ImportError:  # during packaging, docs are moved to html_docs
+    from html_docs import getVersion
 
 
----
+changelog = open('CHANGES.rst').read()
+long_description = "\n\n".join([
+    open('README.rst').read(),
+    open('CONTRIBUTORS.rst').read(),
+    changelog
+])
 
-Závislosti:
-    sh
 
-dependency_links = [
-    "http://xael.org/norman/python/pyclamd/pyClamd-0.3.4.tar.gz"
-]
+class BuildSphinx(sdist):
+    """
+    Generates sphinx documentation, puts it into html_docs/, packs it to
+    package and removes unused directory.
+    """
+    def run(self):
+        d = os.path.abspath('.')
+        DOCS = d + "/" + "docs"
+        DOCS_IN = DOCS + "/_build/html"
+        DOCS_OUT = d + "/html_docs"
+
+        if not self.dry_run:
+            print "Generating the documentation .."
+
+            os.chdir(DOCS)
+            os.system("make clean")
+            os.system("make html")
+
+            if os.path.exists(DOCS_OUT):
+                shutil.rmtree(DOCS_OUT)
+
+            shutil.copytree(DOCS_IN, DOCS_OUT)
+            shutil.copy(DOCS + "/__init__.py", DOCS_OUT)  # for getVersion()
+            os.chdir(d)
+
+        sdist.run(self)
+
+        if os.path.exists(DOCS_OUT):
+            shutil.rmtree(DOCS_OUT)
+
+
+setup(
+    name='edeposit.amqp.ftp',
+    version=getVersion(changelog),
+    description="E-Deposit's wrapper over ProFTPD.",
+    long_description=long_description,
+    url='https://github.com/edeposit/edeposit.amqp.ftp/',
+
+    author='Edeposit team',
+    author_email='edeposit@email.cz',
+
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    license='GPL2+',
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+
+    scripts=['bin/edeposit_amqp_clamd_init.py'],
+
+    namespace_packages=['edeposit', 'edeposit.amqp'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        "sh>=1.09",
+        "pyClamd>=0.3.4"
+    ],
+    extras_require={
+        "test": [
+            "pytest"
+        ],
+        "docs": [
+            "sphinx",
+            "sphinxcontrib-napoleon",
+        ]
+    },
+
+    cmdclass={'sdist': BuildSphinx}
+)
